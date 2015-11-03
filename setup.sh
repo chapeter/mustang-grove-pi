7a3ed949bbc52f5ba43ae81709cb4e7628d35b51
csoukup <csoukup33@gmail.com>
2015-11-02 23:18:51 -0600

    Initial commit with files included

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#Update the Raspbian repos
+sudo apt-get update
+
+#Update the system
+sudo apt-get upgrade
+
+#Install tightvnc packages
+sudo apt-get install tightvncserver
+
+#Copy startup script and enable vncserver
+sudo cp ./vncserver /etc/init.d/vncserver
+sudo chmod 755 /etc/init.d/vncserver
+sudo update-rc.d /etc/init.d/vncserver defaults
+
+#Extract vnc passwd and xstartup files into the pi user's home directory
+tar zxC /home/pi -f vnc_files.tar.gz 
+
+#Copy startup script and enable printing of hostname and ip to Grove Pi LCD connected to I2C port
+sudo cp ./print_ip /etc/init.d/print_ip
+sudo chmod 755 /etc/init.d/print_ip
+sudo update-rc.d /etc/init.d/print_ip defaults
+
+#Clone required projects from github to ~pi
+cd ~pi
+/usr/bin/git clone https://github.com/IoTDevLabs/iot-educ.git
+cd ~pi/iot-educ/rpi
+./install-python-packages.sh
+python read-cpu-temp.py
+sleep 5
+
+cd ~pi
+/usr/bin/git clone https://github.com/DexterInd/GrovePi
+cd GrovePi/Script
+sudo chmod +x install.sh
+sudo ./install.sh
+
+cd ~pi/GrovePi/Software/Python
+sudo python setup.py install
